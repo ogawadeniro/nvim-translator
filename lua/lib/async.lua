da13b8cc0959c56a7a172b80df3dbaa6ac5e92f5
ogawadeniro <ookawa14106@gmail.com>
2026-07-02 09:06:27 +0900

    fix: 非同期処理の実行をjobstartにした

--- lua/lib/async.lua
+++ lua/lib/async.lua
@@ -1,7 +1,34 @@
 local M = {}
 
----@type fun(cmd: string, cmd_args: string[], on_cmd_success: function, on_cmd_err: function)
 M.execute_cmd_async = function(cmd, cmd_args, on_cmd_success, on_cmd_err)
+    local response_chunks = {}
+
+    vim.fn.jobstart({ cmd, unpack(cmd_args) }, {
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+            if data then
+                table.insert(response_chunks, table.concat(data, "\n"))
+            end
+        end,
+
+        -- 通信が終わったタイミングで実行される関数
+        on_exit = function(_, exit_code)
+            -- 【チェック1】curl自体の通信が成功したか (0なら通信成功)
+            if exit_code ~= 0 then
+                vim.schedule(on_cmd_err)
+                vim.notify(cmd .. "での通信に失敗したよ", vim.log.levels.ERROR)
+                return
+            end
+
+            -- レスポンスの結合とJSONデコード
+            local raw_json = table.concat(response_chunks, "")
+            vim.schedule(function() on_cmd_success(raw_json) end)
+        end
+    })
+end
+
+---@type fun(cmd: string, cmd_args: string[], on_cmd_success: function, on_cmd_err: function)
+M._execute_cmd_async = function(cmd, cmd_args, on_cmd_success, on_cmd_err)
     if not vim.fn.executable(cmd) then
         vim.notify(cmd .. " is not executable", vim.log.levels.ERROR)
         return
