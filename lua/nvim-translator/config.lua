a81947cb8b8b7f1984979da3e54457ed1618712c
ogawadeniro <ookawa14106@gmail.com>
2026-07-02 09:04:49 +0900

    fix: コンフィグ構成を変更。APIクライアントの設定を行うようにした。

--- lua/nvim-translator/config.lua
+++ lua/nvim-translator/config.lua
@@ -1,75 +1,49 @@
 local M = {}
 
----nvim-translator configuration type
----@class NTConfig
----@field keymap NTKeymapConfig[]
---@field ui NTUIConfig
-
 -- keymap configuration type
 ---@class NTKeymapConfig
----@field src LANG
----@field dst LANG
+---@field src TRANS_LANG
+---@field dst TRANS_LANG
 ---@field key string
 
--- define nvim-translator ui configuration type
---@class NTUIConfig
---@field border BORDER_TYPE
+---@class NTClientConfig
+---@field provider string
+---@field opt NTClientConfigOpt
 
----default configuration of nvim-translator
----@type NTConfig
-local default_config = {
+---@class NTClientConfigOpt
+---@field url string?
+---@field api_key string?
+
+---@class NTConfig
+---@field keymap NTKeymapConfig[]?
+---@field client NTClientConfig
+local config = {
     keymap = {
         {
+            key = "<Leader>?",
             src = "en",
             dst = "ja",
-            key = "<Leader>?",
         },
         {
+            key = "<Leader>g?",
             src = "ja",
             dst = "en",
-            key = "<Leader>g?",
         }
     },
+    client = {
+        provider = "",
+        opt = {}
+    }
 }
 
----@param user_config NTConfig
----@return boolean is_valid
----@return string err
-local validate_config = function(user_config)
-    local is_valid = true
-    local err = "nvim-translator: invalid userconfig."
-
-    -- check keymap configuration
-    for i = 1, #user_config.keymap do
-        if user_config.keymap[i] == nil then
-            is_valid = false
-            err = err .. " nil value not allowed on keymap configuration."
-            break
-        end
-    end
-
-    if is_valid == true then
-        return true, ""
-    else
-        return false, err
-    end
+-- 設定をビルドする関数
+---@type fun(user_config: NTConfig?)
+function M.build_config(user_config)
+    config = vim.tbl_deep_extend("force", config, user_config)
 end
 
----function to build configuration
----@type fun(user_config: NTConfig?): NTConfig
-function M.build_config(user_config)
-    -- return default config when user config is nil
-    if user_config == nil then
-        return default_config
-    end
-    -- validate user configuration
-    local is_valid_config, err = validate_config(user_config)
-    if is_valid_config == false then
-        vim.notify(err, vim.log.levels.WARN)
-        return default_config
-    end
-
-    return user_config
+function M.get()
+    return config
 end
 
 return M
