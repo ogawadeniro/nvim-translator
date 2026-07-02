dfd0494f448604d4245a2fff6e004878e7254dc4
ogawadeniro <ookawa14106@gmail.com>
2026-07-02 09:07:48 +0900

    fix: 役割をへらした。

--- lua/nvim-translator.lua
+++ lua/nvim-translator.lua
@@ -1,67 +1,61 @@
 local M = {}
 
--- import modules
+-- モジュールをインポート
 local nt_config = require('nvim-translator.config')
 local translator = require('nvim-translator.translator')
-local ui = require('nvim-translator.ui')
 
----@param text string
----@param src LANG
----@param dst LANG
-local translate = function(text, src, dst)
-    -- open floating window
-    ui.new()
-    -- move to top line of floating window.
-    vim.cmd('noautocmd normal! gg0')
-
-    -- draw spinner to notify user now wating
-    local spinner = ui.draw_spinner(
-        { "◐ now translating.", "☻ now translating..", "◑ now translating...", "◎ now translating" }, 1.5)
-
-    -- translate text asynchronously(stop spinner in callback function on_success)
-    local on_success = function(data)
-        if spinner and (not vim.loop.is_closing(spinner)) then
-            spinner:close()
-        end
-        -- TODO resize window size
-        -- ui.resize()
-        local formatted_data = translator.format_text(dst, data)
-        ui.overwrite_lines(formatted_data)
-    end
-    local on_err = function(data)
-        if spinner and (not vim.loop.is_closing(spinner)) then
-            spinner:close()
-            vim.notify("falied to translation\n" .. data, vim.log.levels.WARN)
-        end
-    end
-    translator.translate(text, src, dst, on_success, on_err)
+-- visualモードで選択したテキストを取得する関数
+---@type fun(): string
+local get_selected_text = function()
+    local zreg_bf = vim.fn.getreg("z")
+    vim.cmd('noautocmd normal! "zy')
+    local zreg_af = vim.fn.getreg("z")
+    vim.fn.setreg("z", zreg_bf)
+    return zreg_af
 end
 
--- usage
--- ``` lua
--- require('nvim-translator').setup({
---     keymap = {
---        {
---            src = "en",
---            dst = "ja",
---            key = "<Leader>?",
---        },
---        -- ...add custom keymaps
---     }
--- })
--- ```
+-- カーソル下の単語を取得する関数
+---@type fun(): string
+local get_cursor_text = function()
+    local zreg_bf = vim.fn.getreg("z")
+    vim.cmd('noautocmd normal! viw"zy')
+    local zreg_af = vim.fn.getreg("z")
+    vim.fn.setreg("z", zreg_bf)
+    return zreg_af
+end
+
+local text_loaders = {
+    visual = get_selected_text,
+    cursor = get_cursor_text
+}
+
+-- 翻訳元テキストを取得する関数
+---@param type "visual"|"cursor"
+---@return string
+local get_src_text = function(type)
+    local text_loader = text_loaders[type]
+    local text = text_loader()
+    return text
+end
+
 ---@param user_config NTConfig?
 function M.setup(user_config)
-    -- override default nvim-translator configuration
-    local config = nt_config.build_config(user_config)
-    local keymaps = config.keymap
+    -- コンフィグをビルド
+    nt_config.build_config(user_config)
+    nt_config = nt_config.get()
 
-    -- set keymap
+    -- keymapを設定
+    local keymaps = nt_config.keymap
     for i = 1, #keymaps do
         vim.api.nvim_set_keymap("v", keymaps[i].key, "", {
             callback = function()
-                local text = translator.load_text("visual")
-                translate(text, keymaps[i].src, keymaps[i].dst)
+                local txt = get_src_text("visual")
+                local trans_req = {
+                    txt = txt,
+                    src = keymaps[i].src,
+                    dst = keymaps[i].dst
+                }
+                translator.translate(trans_req, nt_config)
             end,
         })
     end
