e45ae3fc597dbee741ce7370ed739a2f00d58fa5
ogawadeniro <ookawa14106@gmail.com>
2026-07-02 18:22:41 +0900

    fix: 言語型付け

--- lua/nvim-translator/translator.lua
+++ lua/nvim-translator/translator.lua
@@ -9,8 +9,129 @@ local ui = require('nvim-translator.ui')
 
 --- 指定できる言語のタイプ
 ---@alias TRANS_LANG @翻訳時に指定する言語のenum
+---| "ace" Acehnese
+---| "af" Afrikaans
+---| "sq" Albanian
+---| "ar" Arabic
+---| "an" Aragonese
+---| "hy" Armenian
+---| "as" Assamese
+---| "ay" Aymara
+---| "az" Azerbaijani
+---| "ba" Bashkir
+---| "eu" Basque
+---| "be" Belarusian
+---| "bn" Bengali
+---| "bho" Bhojpuri
+---| "bs" Bosnian
+---| "br" Breton
+---| "bg" Bulgarian
+---| "my" Burmese
+---| "yue" Cantonese
+---| "ca" Catalan
+---| "ceb" Cebuano
+---| "zh"-hans Chinese (simplified)
+---| "zh"-hant Chinese (traditional)
+---| "zh" Chinese (unspecified variant)
+---| "hr" Croatian
+---| "cs" Czech
+---| "da" Danish
+---| "prs" Dari
+---| "nl" Dutch
+---| "en" English (all variants)
+---| "en"-us English (American)
+---| "en"-gb English (British)
+---| "eo" Esperanto
+---| "et" Estonian
+---| "fi" Finnish
+---| "fr" French
+---| "fr"-ca French (Canadian)
+---| "gl" Galician
+---| "ka" Georgian
+---| "de" German
+---| "de"-ch German (Swiss)
+---| "el" Greek
+---| "gn" Guarani
+---| "gu" Gujarati
+---| "ht" Haitian Creole
+---| "ha" Hausa
+---| "he" Hebrew
+---| "hi" Hindi
+---| "hu" Hungarian
+---| "is" Icelandic
+---| "ig" Igbo
+---| "id" Indonesian
+---| "ga" Irish
+---| "it" Italian
 ---| "ja" Japanese
----| "en" English
+---| "jv" Javanese
+---| "pam" Kapampangan
+---| "kk" Kazakh
+---| "gom" Konkani
+---| "ko" Korean
+---| "kmr" Kurdish (Kurmanji)
+---| "ckb" Kurdish (Sorani)
+---| "ky" Kyrgyz
+---| "la" Latin
+---| "lv" Latvian
+---| "ln" Lingala
+---| "lt" Lithuanian
+---| "lmo" Lombard
+---| "lb" Luxembourgish
+---| "mk" Macedonian
+---| "mai" Maithili
+---| "mg" Malagasy
+---| "ms" Malay
+---| "ml" Malayalam
+---| "mt" Maltese
+---| "mi" Maori
+---| "mr" Marathi
+---| "mn" Mongolian
+---| "ne" Nepali
+---| "nb" Norwegian Bokmål
+---| "oc" Occitan
+---| "om" Oromo
+---| "pag" Pangasinan
+---| "ps" Pashto
+---| "fa" Persian
+---| "pl" Polish
+---| "pt"-br Portuguese (Brazilian)
+---| "pt"-pt Portuguese (European)
+---| "pt" Portuguese (unspecified variant)
+---| "pa" Punjabi
+---| "qu" Quechua
+---| "ro" Romanian
+---| "ru" Russian
+---| "sa" Sanskrit
+---| "sr" Serbian
+---| "st" Sesotho
+---| "scn" Sicilian
+---| "sk" Slovak
+---| "sl" Slovenian
+---| "es" Spanish
+---| "es"-419 Spanish (Latin American)
+---| "su" Sundanese
+---| "sw" Swahili
+---| "sv" Swedish
+---| "tl" Tagalog
+---| "tg" Tajik
+---| "ta" Tamil
+---| "tt" Tatar
+---| "te" Telugu
+---| "th" Thai
+---| "ts" Tsonga
+---| "tn" Tswana
+---| "tr" Turkish
+---| "tk" Turkmen
+---| "uk" Ukrainian
+---| "ur" Urdu
+---| "uz" Uzbek
+---| "vi" Vietnamese
+---| "cy" Welsh
+---| "wo" Wolof
+---| "xh" Xhosa
+---| "yi" Yiddish
+---| "zu" Zulu
 
 -- 翻訳可能な最大文字数
 ---@type integer
@@ -34,7 +155,7 @@ end
 
 -- 翻訳を実行する関数
 ---@param trans_req NTReqest
----@pram nt_config NTConfig
+---@param nt_config NTConfig
 M.translate = function(trans_req, nt_config)
     -- apiクライアントを呼び出す
     local ok, api_client = pcall(require, "clients." .. nt_config.client.provider)
@@ -44,7 +165,12 @@ M.translate = function(trans_req, nt_config)
     end
 
     -- フローティングウィンドウを開いて一番上の行に移動
-    ui.new()
+    local title =
+        " 󰗊  "
+        .. trans_req.src .. " " .. trans_req.dst .. " "
+        .. nt_config.client.provider
+        .. " "
+    ui.new(title)
     vim.cmd('noautocmd normal! gg0')
 
     -- ロードスピナーを表示する
